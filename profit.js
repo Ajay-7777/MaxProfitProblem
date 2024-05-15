0ff49c1ec988d5dfd4c5c415f10237f52bca4961
Ajay-7777 <65538820+Ajay-7777@users.noreply.github.com>
2024-05-15 19:22:11 +0530

    Update profit.js

--- profit.js
+++ profit.js
@@ -1,46 +1,81 @@
-const testcases = [
-  { time: 7, earning: 3000 },
-  { time: 8, earning: 4500 },
-  { time: 13, earning: 16500 }
-];
 
-
-const CreationTime=[5,4,10]
-const earnings=[1500,1000,3000]
-
-function Solverfunction(inputfromuser){
-    let result = []
-    const givendataforBusiness = [{name:'T', earning:1500, giventime:5}, {name:'P', earning:1000, giventime:4}, {name:'C', earning:3000, giventime:10}]
-    for(let i = 0; i<givendataforBusiness.length; i++){
-        let bussiness = givendataforBusiness[i]
-        if(inputfromuser.time < bussiness.giventime){
-            continue
-        }else{
-            let totalTimeforCompletebusiness = 0;
-            let Totalbusiness = parseInt(inputfromuser.time / bussiness.giventime)
-            let totaldvelopmentTimetaken  = 0;
-            for(let i = 1; i<= Totalbusiness; i++){
-                let timeInbusiness = (inputfromuser.time - totaldvelopmentTimetaken ) - bussiness.giventime
-                totalTimeforCompletebusiness += timeInbusiness
-                totaldvelopmentTimetaken += bussiness.giventime
+function MaxiumProfitCalculate(TotalTime, earning, buildingarr, businesss, probability) {
+  for (let i = 0; i < probability.length; i++) {
+    let currTime = TotalTime;
+    if (currTime - probability[i].time <= 0) {
+      if (earning >= businesss.earning) {
+       
+        if (businesss.built.length === 0)
+          businesss.built.push([...buildingarr]);
+        else if (
+          businesss.earning === earning &&
+          businesss.built.length > 0
+        ) {
+          let flag = true;
+          for (let j = 0; j < businesss.built.length; j++) {
+            if (
+              businesss.built[j][0] === buildingarr[0] &&
+              businesss.built[j][1] === buildingarr[1] &&
+              businesss.built[j][2] === buildingarr[2]
+            ) {
+              flag = false;
+              break;
             }
-            let totalEarningFromBussiness = totalTimeforCompletebusiness * bussiness.earning
-            if(totalEarningFromBussiness == inputfromuser.earning){
-                let solution = {'T':0, 'P':0, 'C':0}
-                solution[bussiness.name] = Totalbusiness
-                result.push(solution)
-            }
+          }
+          if (flag) businesss.built.push([...buildingarr]);
+        } else if (businesss.earning < earning) {
+          businesss.built = [];
+          businesss.built.push([...buildingarr]);
         }
+        businesss.earning = earning;
+      }
+    } else {
+      currTime -= probability[i].time;
+      earning += currTime * probability[i].cost;
+      buildingarr[i] += 1;
+      MaxiumProfitCalculate(currTime, earning, buildingarr, businesss, probability);
+      buildingarr[i] -= 1;
+      earning -= currTime * probability[i].cost;
     }
-    let index=1;
-    result.forEach(res=>{
-        console.log(index++,res);
-    })
-    return result
+  }
+  return;
 }
 
-testcases.forEach(testcase=>{
-    console.log(testcase)
-    Solverfunction(testcase)
-    console.log("")
-})
+function MaximumProfit(TotalTime) {
+  let buildingarr = [0, 0, 0];
+  let businesss = { earning: 0, built: [] };
+  let probability = [
+    { time: 5, cost: 1500 },
+    { time: 4, cost: 1000 },
+    { time: 10, cost: 3000 },
+  ];
+  MaxiumProfitCalculate(TotalTime, 0, buildingarr, businesss, probability);
+  if (businesss["earning"] === 0) {
+    console.log(`T:${0}P:${0}C:${0}`);
+  } else {
+    console.log("Earnings:", businesss.earning);
+    for (let i = 0; i < businesss.built.length; i++) {
+      console.log(
+        `T:${businesss.built[i][0]}P:${businesss.built[i][1]}C:${businesss.built[i][2]}`
+      );
+    }
+  }
+}
+
+
+MaximumProfit(7);
+MaximumProfit(8);
+MaximumProfit(13);
+
+
+
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Input: ", (inp) => {
+MaximumProfit(inp.split(" ").map((dt) => Number(dt))[0]);
+  readline.close();
+});
